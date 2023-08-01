54c0ba30f556882eec11a2d712a09865f9c45f92
Alex Jeronimo <jeronimoalex@gmail.com>
2023-08-01 08:50:32 +0300

    new file added

---
+++ arrays.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
+	/*
+		index := 1
+		fmt.Println(index, notes[index])
+		index = 3
+		fmt.Println(index, notes[index])
+
+		for i := 0; i <= 2; i++ {
+			fmt.Println(i, notes[i])
+		}
+
+		fmt.Println(len(notes))
+		primes := [5]int{2, 3, 5, 7, 11}
+		fmt.Println(len(primes))
+	*/
+
+	for i := 0; i < len(notes); i++ {
+		fmt.Println(i, notes[i])
+	}
+}
