7e89378e22bd55622f7c7e1c4b91d9ccb08ccb65
Alex Jeronimo <jeronimoalex@gmail.com>
2023-08-07 19:07:25 +0300

    added new files

---
+++ files.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func ScanDirectory(path string) {
+	fmt.Println(path)
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		// return err
+		panic(err)
+	}
+
+	for _, file := range files {
+		filePath := filepath.Join(path, file.Name())
+		if file.IsDir() {
+			ScanDirectory(filePath)
+		} else {
+			fmt.Println(filePath)
+		}
+	}
+	//return nil
+}
+
+func main() {
+	/*
+		err := ScanDirectory("C:\\Users\\sysadmin\\go\\src")
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+	ScanDirectory("C:\\Users\\sysadmin\\go\\src")
+}
