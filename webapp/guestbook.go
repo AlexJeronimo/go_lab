c6b7cb295b7b97339c936fca3bec6b7240f8ee44
Alex Jeronimo <jeronimoalex@gmail.com>
2023-08-09 18:54:13 +0300

    webapp completed

--- webapp/guestbook.go
+++ webapp/guestbook.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -59,8 +60,16 @@ func newHandler(writer http.ResponseWriter, request *http.Request) {
 
 func createHandler(writer http.ResponseWriter, request *http.Request) {
 	signature := request.FormValue("signature")
-	_, err := writer.Write([]byte(signature))
+	//_, err := writer.Write([]byte(signature))
+	//check(err)
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
 	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
 }
 
 func main() {
